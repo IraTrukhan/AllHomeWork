8e1667f7530f557f3011e9477ead002a9abf6210
IraTrukhan <iryna.viaz@gmail.com>
2021-11-09 23:54:42 +0200

    add universal functions

--- task1.js
+++ task1.js
@@ -1,9 +1,14 @@
 // You have 2 arrays of integers. Return the total sum of all those integers.
-function sumArrays(array1, array2){
-    return  array1.reduce((sum, current) => sum + current) + 
-        array2.reduce((sum, current) => sum + current);   
+function concatArrays(...arrays){
+    let resultArray = [];
+    for (let array of arrays) resultArray = resultArray.concat(array);
+    return resultArray;
 }
 
-let array1 = [1, 2, 3, 4, 5];
-let array2 = [1, 4, 22, 5];
-alert(sumArrays(array1, array2));
+function sumArrays(...arrays){
+    return  concatArrays(...arrays).reduce((sum, current) => sum + current, 0);}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [1, 4, 22, 5];
+const array3 = [11, 3];
+alert(sumArrays(array1, array2, array3, array1, array2));
