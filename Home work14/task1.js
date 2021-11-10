9f82510c53f6b1f792fa131dc5858b7c699acf73
IraTrukhan <iryna.viaz@gmail.com>
2021-11-10 10:28:46 +0200

    folder destructuring

--- Home work14/task1.js
+++ Home work14/task1.js
@@ -2,13 +2,15 @@
 function concatArrays(...arrays){
     let resultArray = [];
     for (let array of arrays) resultArray = resultArray.concat(array);
+    console.log(resultArray);
     return resultArray;
 }
 
 function sumArrays(...arrays){
-    return  concatArrays(...arrays).reduce((sum, current) => sum + current, 0);}
+    return  concatArrays(...arrays).reduce((sum, current) => sum + current, 0);
+}
 
 const array1 = [1, 2, 3, 4, 5];
 const array2 = [1, 4, 22, 5];
 const array3 = [11, 3];
-alert(sumArrays(array1, array2, array3, array1, array2));
+alert(sumArrays(2, 3, array1, array2, array3, array1, array2, 5));
