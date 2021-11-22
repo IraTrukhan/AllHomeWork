334efe81d3a41b5b06e6f6ee9c4eef29c26102d6
IraTrukhan <iryna.viaz@gmail.com>
2021-11-22 14:17:08 +0200

    added function which return new class

--- Home work19/task2.js
+++ Home work19/task2.js
@@ -1,13 +1,21 @@
 function makeClass(...args){
-    let newClass = {};
-    for(let arg of args){
-        newClass[arg] = arg;
+    let count = 0;
+    return class newClass {        
+        constructor(...parameters){
+            for(let arg of args){
+                this[arg] = parameters[count];
+                count ++;
+            }
+        count = 0;
+        }
     }
-    return newClass;
 }
 const Animal = makeClass("name","species","age","health","weight","color");
-console.log(Animal);
-console.log(typeof (Animal));
-//let rabbit = new Animal("Black","Rabbit", 3, 22, 8,"color");
-  
-  
+let rabbit = new Animal("black","rabbit", 3, 22, 8,"color");
+
+const Car = makeClass("marka","model","year", "color");
+let car1 = new Car('Volswagen', 'Passat', '2012', 'black');
+let rabbit1 = new Animal("white","rabbit", 12, 25, 7,"color");
+console.log(rabbit);
+console.log(rabbit1);
+console.log(car1);
